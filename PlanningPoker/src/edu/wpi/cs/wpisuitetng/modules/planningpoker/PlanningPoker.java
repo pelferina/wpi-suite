27afa6207ce201c4ee7e7c66d30e94c1233dd485
nmbryant <nmbryant@wpi.edu>
2014-03-22 14:35:59 -0400

    test name

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/PlanningPoker.java
@@ -0,0 +1,60 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
+import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+
+public class PlanningPoker implements IJanewayModule{
+
+	/** The tabs used by this module -test2 */
+	private ArrayList<JanewayTabModel> tabs;
+	
+	/**
+	 * Construct a new DummyModule for demonstration purposes
+	 */
+	public PlanningPoker() {
+		
+		// Setup button panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(new JButton("Func A"));
+		buttonPanel.add(new JButton("Func B"));
+		
+		// Setup the main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(new JLabel("Planning Poker"), BorderLayout.PAGE_START);
+		mainPanel.add(new JTextField(), BorderLayout.CENTER);
+		mainPanel.add(new JTextField(), BorderLayout.CENTER);
+		
+		tabs = new ArrayList<JanewayTabModel>();
+		JanewayTabModel tab = new JanewayTabModel("PlanningPoker", new ImageIcon(), buttonPanel, mainPanel);
+		tabs.add(tab);
+	}
+	
+	/*
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
+	 */
+	@Override
+	public String getName() {
+		return "Planning Poker";
+	}
+
+	/*
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
+	 */
+	@Override
+	public List<JanewayTabModel> getTabs() {
+		return tabs;
+	}
+}
