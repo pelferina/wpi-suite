7245082bb5a20f2b77ee662c243f0e9e78644003
Steven Ireland <spireland@wpi.edu>
2014-03-24 19:42:50 -0400

    spireland - sets default newGameWindow size to 450,300

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/newgame/NewGameImportWindow.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/newgame/NewGameImportWindow.java
@@ -36,18 +36,19 @@ public class NewGameImportWindow extends JFrame {
 	
 	private void setupPanel()
 	{
-		add(mainPanel);
+		getContentPane().add(mainPanel);
 		SpringLayout sl_mainPanel = new SpringLayout();
+		sl_mainPanel.putConstraint(SpringLayout.NORTH, reqList, 42, SpringLayout.NORTH, mainPanel);
+		sl_mainPanel.putConstraint(SpringLayout.SOUTH, reqList, -44, SpringLayout.NORTH, finishButton);
 		sl_mainPanel.putConstraint(SpringLayout.WEST, finishButton, 178, SpringLayout.WEST, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.SOUTH, finishButton, -25, SpringLayout.SOUTH, mainPanel);
 		mainPanel.setLayout(sl_mainPanel);
 		reqList.setListData(listValue);
 		mainPanel.add(finishButton);
-		sl_mainPanel.putConstraint(SpringLayout.NORTH, reqList, 42, SpringLayout.NORTH, mainPanel);
 		sl_mainPanel.putConstraint(SpringLayout.WEST, reqList, 62, SpringLayout.WEST, mainPanel);
-		sl_mainPanel.putConstraint(SpringLayout.SOUTH, reqList, -44, SpringLayout.NORTH, finishButton);
 		sl_mainPanel.putConstraint(SpringLayout.EAST, reqList, -78, SpringLayout.EAST, mainPanel);
 		mainPanel.add(reqList);
+		setSize(450,300);
 		setVisible(true);
 	}
 }
