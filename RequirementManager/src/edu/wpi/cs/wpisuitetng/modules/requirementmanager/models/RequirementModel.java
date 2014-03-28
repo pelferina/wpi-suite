c71116b10e466448b633078653b84a0f80689bed
Rafi Hayne <rafi@ubuntu.(none)>
2014-03-28 18:13:14 -0400

    removed unnecessary print statements

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/RequirementModel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/models/RequirementModel.java
@@ -204,7 +204,6 @@ public class RequirementModel extends AbstractListModel{
 	
 	 * @return the requirements held within the requirementmodel. */
 	public List<Requirement> getRequirements() {
-		System.out.println("getrequirements"+requirements.size());
 		return requirements;
 	}
 
