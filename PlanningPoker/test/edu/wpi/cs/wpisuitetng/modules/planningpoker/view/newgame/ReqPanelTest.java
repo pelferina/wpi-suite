9661656d2bdb3788b5f1071c46648a67cf89556d
nmbryant <nmbryant@wpi.edu>
2014-04-14 19:08:50 -0400

    nmbryant adds a test for the newGameReqPanel

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/newgame/ReqPanelTest.java
@@ -0,0 +1,60 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.newgame;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+
+public class ReqPanelTest {
+	
+	@Test
+	public void testAddRemoveButtons(){
+		Requirement req1 = new Requirement();
+		Requirement req2 = new Requirement();
+		Requirement req3 = new Requirement();
+		List<Requirement> reqs = new ArrayList<Requirement>();
+		reqs.add(req1);
+		reqs.add(req2);
+		reqs.add(req3);
+		NewGameReqPanel reqPanel = new NewGameReqPanel(reqs);
+		JTable unselected = reqPanel.getReqsTable();
+		JTable selected = reqPanel.getSelectedTabel();
+		JButton addOne = reqPanel.getAddOneButton();
+		JButton addAll = reqPanel.getAddAllButton();
+		JButton removeOne = reqPanel.getRemoveOneButton();
+		JButton removeAll = reqPanel.getRemoveAllButton();
+		unselected.setRowSelectionInterval(0, 1);
+		addOne.doClick();
+		List<Requirement> selectedReqs = reqPanel.getSelected();
+		assertTrue(selectedReqs.contains(req1));
+		assertFalse(selectedReqs.contains(req2));
+		addOne.doClick();
+		selectedReqs = reqPanel.getSelected();
+		assertTrue(selectedReqs.contains(req2));
+		selected.setRowSelectionInterval(0, 1);
+		removeOne.doClick();
+		selectedReqs = reqPanel.getSelected();
+		assertTrue(selectedReqs.contains(req2));
+		assertFalse(selectedReqs.contains(req1));
+		removeOne.doClick();
+		selectedReqs = reqPanel.getSelected();
+		assertTrue(selectedReqs.size() == 0);
+		addAll.doClick();
+		selectedReqs = reqPanel.getSelected();
+		assertTrue(selectedReqs.size() == 3);
+		removeAll.doClick();
+		selectedReqs = reqPanel.getSelected();
+		assertFalse(selectedReqs.contains(req1));
+		assertFalse(selectedReqs.contains(req2));
+		assertFalse(selectedReqs.contains(req3));
+	}
+	
+}
