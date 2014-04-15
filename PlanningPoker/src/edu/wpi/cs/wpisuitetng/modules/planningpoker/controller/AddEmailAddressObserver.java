cffbf58eb83ecd0a8c9480c37376f12fed5aa96a
esfaust <esfaust@gmail.com>
2014-04-14 22:28:06 -0400

    Recommenting of AddEmailAddressObserver

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddEmailAddressObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddEmailAddressObserver.java
@@ -1,13 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013 -- WPI Suite
+ * Copyright (c) 2014 -- WPI Suite
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Chris Casola
  ******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
@@ -21,7 +19,8 @@ import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
  * This observer is called when a response is received from a request
  * to the server to add an email.
  * 
- * @author Chris Casola
+ * @author Cosmic Latte	
+ * @version $Revision: 1.0 $
  *
  */
 public class AddEmailAddressObserver implements RequestObserver {
