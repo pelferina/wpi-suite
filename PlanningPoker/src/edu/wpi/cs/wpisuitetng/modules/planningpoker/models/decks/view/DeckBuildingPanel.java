79235262819c7c4a5fe1541753664ff65ad6bac2
ShadowyLuke <luke.lebrao@gmail.com>
2014-04-24 20:08:30 -0400

    lrlebrao nmbryant Sets up GUI for DeckBuildingPanel

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/decks/view/DeckBuildingPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/decks/view/DeckBuildingPanel.java
@@ -24,6 +24,7 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.decks.Deck;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.decks.DeckModel;
 
 import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -33,61 +34,137 @@ import java.awt.event.ActionEvent;
 
 @SuppressWarnings({"serial"})
 public class DeckBuildingPanel extends JPanel {
-	private JTextField textField;
-	SpringLayout springLayout = new SpringLayout();
-	JLabel lblDeckName = new JLabel("Deck Name:");
-	JButton btnAddCard = new JButton("Add Card");
-	JButton btnRmvSelected = new JButton("Remove Selected");
-	JButton btnRmvAll = new JButton("Remove all");
+
+	private JButton btnAddCard = new JButton("Add Card");
+	private JButton btnRmvSelected = new JButton("Remove Selected");
+	private JButton btnRmvAll = new JButton("Remove all");
+	private JButton btnSave = new JButton("Save deck");
+	private JButton btnDelete = new JButton("Delete deck");
+	private JComboBox<String> comboBoxDeckList = new JComboBox<String>();
+	private JLabel lblDeckName = new JLabel("Deck Name:");
+	private JLabel lblDecks = new JLabel("Decks:");
+	private JPanel cardPanel = new JPanel();
+	private JScrollPane cardArea = new JScrollPane(cardPanel);
+	private JTextField textField = new JTextField();
+	private SpringLayout springLayout = new SpringLayout();
 	
 	/** Constructor for a DeckPanel panel
 	 */
 	public DeckBuildingPanel(){
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		// Sets up Combo Box for decks
+		setUpDeckList();
 		
-		Font size = new Font(btnRmvSelected.getFont().getName(), btnRmvSelected.getFont().getStyle(), 10);
+		// Sets a consistent font for all buttons
+		Font size = new Font(btnSave.getFont().getName(), btnSave.getFont().getStyle(), 10);
+		
+		btnSave.setFont(size);
+		btnSave.setSize(80, 20);
+		btnDelete.setFont(size);
+		btnDelete.setSize(80, 20);
+		btnAddCard.setFont(size);
+		btnAddCard.setSize(80, 20);
 		btnRmvSelected.setFont(size);
 		btnRmvSelected.setSize(80, 20);
 		btnRmvAll.setFont(size);
 		btnRmvAll.setSize(80, 20);
 		
-		btnAddCard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		// Sets up cardArea
+		cardArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		
+		
+		// All listeners and their functions
+		btnSave.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// TODO: Action corresponding to this
 			}
 		});
 		
-		//Spring layout for textField
-		springLayout.putConstraint(SpringLayout.NORTH, textField, 0, SpringLayout.NORTH, lblDeckName);
-		springLayout.putConstraint(SpringLayout.WEST, textField, 10, SpringLayout.EAST, lblDeckName);
+		btnAddCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO: Action corresponding to this
+			}
+		});
 		
+		btnRmvSelected.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// TODO: Action corresponding to this
+			}
+		});
+		
+		btnRmvAll.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// TODO: Action corresponding to this
+			}
+		});
+		
+		comboBoxDeckList.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// TODO: Action corresponding to this
+			}
+		});
+		
+		//Spring layout for lblDecks
+		springLayout.putConstraint(SpringLayout.NORTH, lblDecks, 20, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblDecks, 20, SpringLayout.WEST, this);
+		
+		//Spring layout for comboBoxDeckList
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, comboBoxDeckList, 0, SpringLayout.VERTICAL_CENTER, lblDecks);
+		springLayout.putConstraint(SpringLayout.WEST, comboBoxDeckList, 10, SpringLayout.EAST, lblDecks);
+		springLayout.putConstraint(SpringLayout.EAST, comboBoxDeckList, 100, SpringLayout.WEST, comboBoxDeckList);
+
 		//Spring layout for lblDeckName
 		springLayout.putConstraint(SpringLayout.NORTH, lblDeckName, 20, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, lblDeckName, 20, SpringLayout.WEST, this);
 	
-		//Spring layout for btnAddCard
-		springLayout.putConstraint(SpringLayout.NORTH, btnAddCard, 0, SpringLayout.NORTH, lblDeckName);
-		springLayout.putConstraint(SpringLayout.WEST, btnAddCard, 30, SpringLayout.EAST, textField);
+		//Spring layout for textField
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, textField, 0, SpringLayout.VERTICAL_CENTER, lblDeckName);
+		springLayout.putConstraint(SpringLayout.WEST, textField, 10, SpringLayout.EAST, lblDeckName);
+		springLayout.putConstraint(SpringLayout.EAST, textField, 100, SpringLayout.WEST, textField);
 		
+		//Spring layout for btnSave
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, btnSave, 0, SpringLayout.VERTICAL_CENTER, lblDeckName);
+		springLayout.putConstraint(SpringLayout.EAST, btnSave, -20, SpringLayout.EAST, this);
+	
+		//Spring layout for btnDelete
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, btnDelete, 0, SpringLayout.VERTICAL_CENTER, lblDeckName);
+		springLayout.putConstraint(SpringLayout.WEST, btnDelete, 10, SpringLayout.EAST, btnSave);
+	
+		//Spring layout for cardArea
+		springLayout.putConstraint(SpringLayout.NORTH, cardArea, 10, SpringLayout.SOUTH, textField);
+		springLayout.putConstraint(SpringLayout.SOUTH, cardArea, 130, SpringLayout.NORTH, cardArea);
+		springLayout.putConstraint(SpringLayout.WEST, cardArea, 20, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.EAST, cardArea, -20, SpringLayout.EAST, this);
+
 		//Spring layout for btnRmvSelected
-		springLayout.putConstraint(SpringLayout.NORTH, btnRmvSelected, 40, SpringLayout.SOUTH, lblDeckName);
-		springLayout.putConstraint(SpringLayout.WEST, btnRmvSelected, 0, SpringLayout.WEST, lblDeckName);
+		springLayout.putConstraint(SpringLayout.NORTH, btnRmvSelected, 10, SpringLayout.SOUTH, cardArea);
+		springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, btnRmvSelected, 0, SpringLayout.HORIZONTAL_CENTER, this);
+		
+		//Spring layout for btnAddCard
+		springLayout.putConstraint(SpringLayout.NORTH, btnAddCard, 0, SpringLayout.NORTH, btnRmvSelected);
+		springLayout.putConstraint(SpringLayout.EAST, btnAddCard, -10, SpringLayout.WEST, btnRmvSelected);
 		
 		//Spring layout for btnRmvAll
-		springLayout.putConstraint(SpringLayout.NORTH, btnRmvAll, 5, SpringLayout.SOUTH, btnRmvSelected);
-		springLayout.putConstraint(SpringLayout.WEST, btnRmvAll, 0, SpringLayout.WEST, btnRmvSelected);
-		springLayout.putConstraint(SpringLayout.EAST, btnRmvAll, 0, SpringLayout.EAST, btnRmvSelected);
+		springLayout.putConstraint(SpringLayout.NORTH, btnRmvAll, 0, SpringLayout.NORTH, btnRmvSelected);
+		springLayout.putConstraint(SpringLayout.WEST, btnRmvAll, 10, SpringLayout.EAST, btnRmvSelected);
 		
 		setLayout(springLayout);
 		
-		add(textField);
 		add(lblDeckName);
+		add(textField);
+		add(btnSave);
+		add(cardArea);
 		add(btnAddCard);
 		add(btnRmvAll);
 		add(btnRmvSelected);
 	}
+	
+	private void setUpDeckList(){
+		//TODO: Populate Combo Box with all existing Deck Names
+	}
+	
 }
+
 
 
 
